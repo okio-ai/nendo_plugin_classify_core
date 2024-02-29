caef03ae5da99220d3b8bdbd447e33783d812c52
faradox <felix@okio.ai>
2024-02-29 20:36:20 +0100

    chore: prepare release 0.2.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 if __name__ == "__main__":
     setup(
         name="nendo-plugin-classify-core",
-        version="0.2.7",
+        version="0.2.8",
         description="Nendo core classification plugin",
         author="Okio Team <team@okio.ai>",
     )
